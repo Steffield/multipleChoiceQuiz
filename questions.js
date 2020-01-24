3d82a7f3026c59e6b21976c556de005070e67d3a
Steffield <steffilebby@gmail.com>
2020-01-24 08:30:17 -0500

    version 2

--- questions.js
+++ questions.js
@@ -2,26 +2,31 @@ var questions = [
   {
     title: "A script that executes when the user clicks the mouse button is an example of what?",
     choices: ["An object", "An event handler", "An impossibility", "A method"],
-    answer: "An event handler"
+    //answer: "An event handler"
+    answer: "choice2"
   },
   {
     title: "What is one way to write JavaScript single-line comments?",
     choices: ["##", ";", "//", "<!--"],
-    answer: "//"
+    //answer: "//"
+    answer: "choice3"
   },
   {
     title: "Which of the following can a JavaScript function do?",
     choices: ["Accept parameters", "Return a value", "Write data to disk", "Both 1 and 2"],
-    answer: "Both 1 and 2"
+    //answer: "Both 1 and 2"
+    answer: "choice4"
   },
   {
     title: "What does JSON stand for?",
     choices: ["JavaScript Object Notation", "Jason, the inventor of JavaScript", "JavaScript Only Numbers", "JavaScript One to Nine"],
-    answer: "JavaScript Object Notation"
+    //answer: "JavaScript Object Notation"
+    answer: "choice1"
   },
   {
     title: "How do you generate a random number in JavaScript?",
     choices: ["Math.randomNum()", "Rand()", "Math.floor()", "Math.random()"],
-    answer: "Math.random()"
+    //answer: "Math.random()"
+    answer: "choice4"
   }
 ]; 
--- script2.js
+++ script2.js
@@ -8,50 +8,50 @@
     //final score= initials : time remaining in local storage and show in last high score div
     // go back and clear buttons on last page
 
-
-//Buttons
-var start = document.getElementById("start-btn");
+//global scopre variables
+//Buttons start page
+var start = document.querySelector(".start-btn");
 var controls=document.querySelector(".controls");
+
 //Header
 var timeEl = document.getElementById("time");
 var highScore =document.getElementById("score");
+
 //Container
 var startCon = document.getElementById("startContainer");
 var questionCon= document.getElementById("questionContainer");
 var lastCon =document.getElementById("highscoreContainer");
 var question=document.getElementById("question");
 var scoreCon= document.getElementById("scoreContainer");
+var correctCon = document.getElementById("feedbackCorrect");
+var wrongCon =document.getElementById("feedbackWrong");
+
 //Timer
 var secondsLeft = 0;
+
 //score
 var questionsCorrect= 0;
-// choice buttons
-var choiceParent = document.querySelectorAll("btn-grid");
-
-var choice1 = document.getElementById("choice1");
-var choiceBtn1= document.getElementById("btn1");
-
-var choice2 = document.getElementById("choice2");
-var choiceBtn2 = document.getElementById("btn2");
-
-var choice3 = document.getElementById("choice3");
-var choiceBtn3 = document.getElementById("btn3");
-
-var choice4 = document.getElementById("choice4");
-var choiceBtn4 = document.getElementById("btn4");
-
-// next back button
-//var next = document.getElementById("next-btn");
-//var back = document.getElementById("back-btn");
-//last buttons
-var goBackBtn = document.getElementById("goBack-btn");
-var clearBtn = document.getElementById("clear-btn");
-//score variable
 var timeRemain=secondsLeft;
 
 
+// choice buttons
+var choiceParent = document.querySelectorAll("btn-grid");
+var choice1 = document.getElementById("choice1");
+var choice2 = document.getElementById("choice2");
+var choice3 = document.getElementById("choice3");
+var choice4 = document.getElementById("choice4");
 
-//navbar time starts run when start is clicked
+// next+ back button
+//var next = document.getElementById("next-btn");
+//var back = document.getElementById("back-btn");
+
+//last page buttons
+var goBackBtn = document.getElementById("goBack-btn");
+var clearBtn = document.getElementById("clear-btn");
+
+
+
+//Function Timer: time starts run in navbar when start is clicked
 function setTime (){
     secondsLeft=75;
     var timerInterval = setInterval(function() {
@@ -66,26 +66,18 @@ function setTime (){
     }, 1000);
 }
 
-//navbar score shows when start is clicked
-function setScore(){
-    console.log("Navbar scorefunction works");
-    /*highScore.textContent= "Score: " + questionsCorrect;
-    if(questionsCorrect===5) {
-        alert("Good job! You answered all questions correctly!");
-    */
-   highScore.textContent= "Score: " + timeRemain;
-    }    
+//Fuction display score: navbar score shows when start is clicked
+// function setScore(){
+//     console.log("Navbar scorefunction runs");
+//     highScore.innerHTML= "Score: " + timeRemain;
+// }    
 
-
+//call functions on click
 start.addEventListener("click", setTime);
-//Score in navbar to 0
-start.addEventListener("click", setScore);
+//start.addEventListener("click", setScore);
 
 //startCon disappears and first question in question container appears when start clicked
 start.addEventListener("click", startQuestions);
-
-//var lastQuestion=questions.length-1;
-//currentQuestion=0;
 
 function startQuestions() {
     console.log("questionfunction");
@@ -100,49 +92,92 @@ function startQuestions() {
         question.innerHTML=questions[i].title;
         choice1.innerHTML=questions[i].choices[0];
         choice2.innerHTML=questions[i].choices[1];
-        choice3.textContent=questions[i].choices[2];
-        choice4.textContent=questions[i].choices[3];
-        //next question if 15sec passed
-        var timeForQuestion= setTimeout(() => console.log("Question should change now!"), 15000);
-        timeForQuestion= setTimeout(i++, 15000);
+        choice3.innerHTML=questions[i].choices[2];
+        choice4.innerHTML=questions[i].choices[3];
 
-        //add to if statement 
-        choiceBtn1.addEventListener("click", function(){
-            if(questions[i].choices[0]==questions[i].answer){
+        //move to next question after 15sec
+        var timerLimit = timeLimit(function() {
+            var timForQuestion= 15;
+            if(timeForQuestion==0){
                 i++;
-            }else if (questions[i].choices[0]!==questions[i].answer){
-                secondsLeft= secondsLeft-10;
             }
-        });
-        choiceBtn2.addEventListener("click", function(){
-            if(questions[i].choices[1]==questions[i].answer){
-                i++;
-            }else if (questions[i].choices[1]!==questions[i].answer){
-                secondsLeft= secondsLeft-10;
-            }
-        });
-        choiceBtn3.addEventListener("click", function(){
-            if(questions[i].choices[2]==questions[i].answer){
-                i++;
-            }else if (questions[i].choices[2]!==questions[i].answer){
-                secondsLeft= secondsLeft-10;
-            }
-        });
-        choiceBtn4.addEventListener("click", function(){
-            if(questions[i].choices[3]==questions[i].answer){
-                i++;
-            }else if (questions[i].choices[3]!==questions[i].answer){
-                secondsLeft= secondsLeft-10;
-            }
-        });
+        }, 1000);
+        timeLimit();
 
+       
     }
-
 }
 
+ //selected buttons 
+ document.addEventListener("click", function(event) {
+    console.log(event.target.id);
+    if(event.target.id==questions.answer){
+        //score+=1;
+        startCon.setAttribute("style", "display: none;");
+        correctCon.setAttribute("style", "display: block;");
+        i++
+        //var i=1;
+        var q= questions[i];
+        question.innerHTML=(i+1) + ". " + q.title;
+        choice1.innerHTML=q.choices[0];
+        choice2.innerHTML=q.choices[1];
+        choice3.innerHTML=q.choices[2];
+        choice4.innerHTML=q.choices[3];
+    } else if (event.target.id !== questions.answer){
+        correctCon.setAttribute("style", "display: none;");
+        wrongCon.setAttribute("style", "display: block;");
+        secondsLeft-=10;
+    }
+});
+        
+        // choiceBtn1.addEventListener("click", function(){
+        //     if(questions[i].choices[0]==questions[i].answer){
+        //         i++;
+        //     }else if (questions[i].choices[0]!==questions[i].answer){
+        //         secondsLeft= secondsLeft-10;
+        //     }
+        // });
+        // choiceBtn2.addEventListener("click", function(){
+        //     if(questions[i].choices[1]==questions[i].answer){
+        //         i++;
+        //     }else if (questions[i].choices[1]!==questions[i].answer){
+        //         secondsLeft= secondsLeft-10;
+        //     }
+        // });
+        // choiceBtn3.addEventListener("click", function(){
+        //     if(questions[i].choices[2]==questions[i].answer){
+        //         i++;
+        //     }else if (questions[i].choices[2]!==questions[i].answer){
+        //         secondsLeft= secondsLeft-10;
+        //     }
+        // });
+        // choiceBtn4.addEventListener("click", function(){
+        //     if(questions[i].choices[3]==questions[i].answer){
+        //         i++;
+        //     }else if (questions[i].choices[3]!==questions[i].answer){
+        //         secondsLeft= secondsLeft-10;
+        //     }
+            
+        // });
+ 
+
+    //next question if 15sec passed
+    // var timeForQuestion= setTimeout(() => console.log("Question should change now!"), 15000);
+    // timeForQuestion= setTimeout(i++, 15000);
+    // setTimeout(function(){
+    //     i++;
+    // },15000);
+
+
+//Feedback to cliked question
+    
+
+        //wrongCon or correctCon should only display 2seconds or so
+
+
 var initials= document.getElementById("initialText");
-// reset final score when calculation ok
-var finalScore=5;
+// the final score is determined by how much time is left when quiz is over
+var finalScore=timeRemain;
 var submit =document.getElementById("submit-btn");
 
 
@@ -156,10 +191,12 @@ function scoreReport(){
     //show final score
 
     //store initals and score
-
     localStorage.setItem("initials", JSON.stringify(initials));
     
-    var user= initials +" : Score = "+ timeRemain;
+    // get most recent submission
+    var lastUser = JSON.parse(localStorage.getItem("initials"));
+    
+    var user= lastUser +" : Score = "+ timeRemain;
     console.log(user);    
     
 }
@@ -178,5 +215,3 @@ function highScoreReport(){
     //finalScore.textContent = lastUser.finalScore;
 
 }
-
-   
